72a1c8629d829904ac68e901df17cd6856b468ff
Junnior Cueva <laiger_live_01@hotmail.com>
2025-08-17 16:49:47 -0500

    Termino del amigo secreto

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,31 +1,25 @@
-//Lista donde se almacenan los nombres ingresados.
-let amigos = [];
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
 
-//implementa una funcion para agregar amigos.
 function agregarAmigo() {
-  let nombreAmigo = document.getElementById("amigo").value;
-  if (validarNombre(nombreAmigo)) {
-    //continuar el guardado de datos
-    amigos.push(nombreAmigo);
-    document.getElementById("amigo").value = "";
-  } else {
-    alert("Por favor, inserte un monbre.");
+  const nombre = inputAmigo.value.trim(); // elimina los espacios en blanco
+  if (nombre === "") {
+    alert("⚠️ Debes ingresar un nombre válido.");
+    return;
   }
+  listaAmigos.push(nombre);
+  //ulListaAmigos.innerHTML = ulListaAmigos.innerHTML + inputAmigo.value;
+  ulListaAmigos.innerHTML += `<li>${nombre}</li>`;
+  inputAmigo.value = ""; // limpiar el input depués de agregar
 }
-
-function validarNombre(amigo) {
-  if (amigo === "") {
-    return false;
-  } else {
-    return true;
+function sortearAmigo() {
+  if (listaAmigos.length === 0) {
+    alert("⚠️ No hay amigos registrados para sortear.");
+    return;
   }
-}
-
-function agregarLista() {
-  let lista = document.getElementById("listaAmigos");
-  lista.innerHTML = "";
-  for (let index = 0; index < amigos.length; index++) {
-    lista.innerHTML = lista.innerHTML 
-  }
-    
+  const random = Math.floor(Math.random() * listaAmigos.length);
+  const amigoSecreto = listaAmigos[random];
+  ulResultado.innerHTML = `<li>🎉El amigo secreto es: ${amigoSecreto}</li>`;
 }
